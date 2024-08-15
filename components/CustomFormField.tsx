fa78b80bdfe5a801f65193ef01fb9f501d340dd3
Andrei <a.bucur@vermeerit.com>
2024-08-15 22:58:47 +0300

    update

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/patientForm";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const RenderField = () => {
+  return <Input className="w-full" type="text" placeholder="Placeholder" />;
+};
+const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
